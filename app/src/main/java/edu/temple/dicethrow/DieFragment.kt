e3509dcf7ebc8b54e0594fbb2aee522680a4683d
karl <karl.morris@temple.edu>
2024-10-17 09:39:53 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -0,0 +1,49 @@
+package edu.temple.dicethrow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlin.random.Random
+
+class DieFragment : Fragment() {
+
+    val DIESIDE = "sidenumber"
+
+    lateinit var dieTextView: TextView
+
+    var dieSides: Int = 6
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            it.getInt(DIESIDE).run {
+                dieSides = this
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_die, container, false).apply {
+            dieTextView = findViewById(R.id.dieTextView)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        throwDie()
+        view.setOnClickListener{
+            throwDie()
+        }
+    }
+
+    fun throwDie() {
+        dieTextView.text = Random.nextInt(dieSides).toString()
+    }
+}
